203fad93b7bebfcc5fc24387f175d8f1a745dafe
RION <ehdtmxk123@naver.com>
2024-12-04 02:46:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,31 @@ from PIL import Image
 import random
 
 # Streamlit 웹 앱 제목 설정
-st.title("Upload Image and Comment")
-
-# 사이드바에 랜덤 이미지 3개 표시
-def display_random_images(uploaded_images):
-    if len(uploaded_images) >= 3:
-        random_images = random.sample(uploaded_images, 3)
-        for image in random_images:
-            st.sidebar.image(image, caption="Random Image", use_column_width=True)
+st.title("What's Happening in Korea?: News & Photos on the Declaration of Martial Law")
 
 # 이미지 업로드
 uploaded_files = st.file_uploader("Choose images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
 # 코멘트 입력
-comment = st.text_input("Enter your comment:")
+comment = st.text_input("Enter your comment:(한마디 입력해주세요)")
 
 # 이미지가 업로드된 경우
 if uploaded_files:
     # 업로드된 이미지들을 리스트로 저장
     uploaded_images = [Image.open(file) for file in uploaded_files]
 
-    # 랜덤 이미지 3개 사이드바에 표시
-    display_random_images(uploaded_images)
-
+    # 사이드바에 랜덤 이미지 3개 표시
+    if len(uploaded_images) >= 3:
+        random_images = random.sample(uploaded_images, 3)  # 3개의 랜덤 이미지 선택
+        for image in random_images:
+            st.sidebar.image(image, caption="Random Image", use_column_width=True)
+    
     # 페이지네이션: 이미지 리스트와 페이지 정보 처리
     per_page = 15
     total_pages = len(uploaded_images) // per_page + (1 if len(uploaded_images) % per_page > 0 else 0)
 
     # 페이지 선택
-    page = st.number_input("Choose a page:", min_value=1, max_value=total_pages, value=1)
+    page = st.number_input("Choose a page:(To view previous photos, please adjust the page number)", min_value=1, max_value=total_pages, value=1)
 
     # 현재 페이지의 이미지들 표시
     start_idx = (page - 1) * per_page
