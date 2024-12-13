a91f6892ddbcabeacee418343805e6787dabe598
RIPPER <ehdtmxk123@gmail.com>
2024-12-13 23:01:46 +0900

    ver1.0.1 commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import io
 load_dotenv()
 client_id = os.environ.get('NAVER_CLIENT_ID')
 client_secret = os.environ.get('NAVER_CLIENT_SECRET')
-
+clova_url = os.environ.get('NAVER_URL')
 # Translator 객체 생성
 # CompletionExecutor 클래스 정의
 class CompletionExecutor:
@@ -29,7 +29,7 @@ class CompletionExecutor:
         }
 
         conn = http.client.HTTPSConnection(self._host)
-        conn.request('POST', '/testapp/v1/api-tools/summarization/v2/40b7767cc1d846e3bf2bde48ccb690a5', json.dumps(completion_request), headers)
+        conn.request('POST', '/testapp/v1/api-tools/summarization/v2/{clova_url}', json.dumps(completion_request), headers)
         response = conn.getresponse()
         result = json.loads(response.read().decode(encoding='utf-8'))
         conn.close()
@@ -93,7 +93,7 @@ def translate_clova(text):
 
     translation_response = completion_executor.execute_translation(translate_request_data)
     if 'choices' in translation_response:
-        return translation_response['choices'][0]['message']['content']
+        return translation_response['result']['text']
     else:
         st.write("Translation:", translation_response)
 
@@ -114,22 +114,9 @@ def search_news(query="계엄", display=8):
         st.error(f"Failed to search news. Status code: {response.status_code}")
         return []
 
-# 이미지 다운로드 함수
-def download_image(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return Image.open(io.BytesIO(response.content))
-        else:
-            return None
-    except Exception as e:
-        st.error(f"Failed to download image: {e}")
-        return None
-
-
 # Streamlit 애플리케이션
 st.title("What's happening in Korea? News and photos related to martial law")
-st.subheader("This website is intended to share news related to the declaration of martial law in South Korea on December 2, 2024 I want to raise awareness about the critical situation in South Korea. I'm urgently seeking help. If you can assist, please contact me at stormkingrank1@gmail.com.")
+st.write("This website is intended to share news related to the declaration of martial law in South Korea on December 2, 2024 I want to raise awareness about the critical situation in South Korea. I'm urgently seeking help. If you can assist, please contact me at stormkingrank1@gmail.com.")
 # 검색 버튼 클릭 횟수 제한
 if 'search_count' not in st.session_state:
     st.session_state['search_count'] = 0
@@ -141,72 +128,12 @@ if st.button("Search"):
         for item in news_items:
             st.subheader(item['title'])
             #st.write(item['description'])
-            
-            # 이미지 불러오기
-            if 'image' in item and item['image']:
-                image = download_image(item['image'])
-                if image:
-                    st.image(image, caption=item['title'], use_column_width=True)
-            
             # 클로바 X 요약 함수 사용
             summary = summarize_clova(item['description'])
             st.write("Summary:", summary)
-            
             # 클로바 X 번역 함수 사용
             #translated_summary = translate_clova(summary)
             #st.write("Translated Summary:", translated_summary)
-            
             st.write(f"[Original Article]({item['link']})")
     else:
         st.warning("You have reached the maximum number of searches (3).")
-        
-        
-        
-=======
-from PIL import Image
-import random
-
-# Streamlit 웹 앱 제목 설정
-st.title("What's Happening in Korea?: News & Photos on the Declaration of Martial Law")
-
-# 이미지 업로드
-uploaded_files = st.file_uploader("Choose images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
-
-# 코멘트 입력
-comment = st.text_input("Enter your comment:(한마디 입력해주세요)")
-
-# 이미지가 업로드된 경우
-if uploaded_files:
-    # 업로드된 이미지들을 리스트로 저장
-    uploaded_images = [Image.open(file) for file in uploaded_files]
-
-    # 사이드바에 랜덤 이미지 3개 표시
-    latest_images = uploaded_images[-3:]  # 업로드된 순서대로 마지막 3개 이미지
-    # 사이드바에 최신 3개 이미지 표시
-    for image in latest_images:
-        st.sidebar.image(image, caption="Latest Image", use_column_width=True)
-    
-    # 페이지네이션: 이미지 리스트와 페이지 정보 처리
-    per_page = 15
-    total_pages = len(uploaded_images) // per_page + (1 if len(uploaded_images) % per_page > 0 else 0)
-
-    # 페이지 선택
-    page = st.number_input("Choose a page:(To view previous photos, please adjust the page number)", min_value=1, max_value=total_pages, value=1)
-
-    # 현재 페이지의 이미지들 표시
-    start_idx = (page - 1) * per_page
-    end_idx = min(page * per_page, len(uploaded_images))
-
-    st.write(f"Showing images {start_idx + 1} to {end_idx} of {len(uploaded_images)}")
-
-    for image in uploaded_images[start_idx:end_idx]:
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    # 코멘트가 있을 경우 출력
-    if comment:
-        st.write(f"Your Comment: {comment}")
-    else:
-        st.write("Please enter a comment.")
-else:
-    st.write("Please upload images and enter a comment.")
->>>>>>> 230ae12730a564a26c817ac82f5e19fc71b0f451
