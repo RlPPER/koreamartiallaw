59b729a9c2c053e6f7bdac0f45d85be195cc73b9
RION <ehdtmxk123@naver.com>
2025-01-03 19:21:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
-import requests
+import pandas as pd
 import json
-import http.client
+import requests
 import os
-#from dotenv import load_dotenv
-#load_dotenv()
-client_id = st.secrets['NAVER_CLIENT_ID']
+import re
 #client_id = os.environ.get('NAVER_CLIENT_ID')
-client_secret = st.secrets['NAVER_CLIENT_SECRET']
 #client_secret = os.environ.get('NAVER_CLIENT_SECRET')
-# Translator 객체 생성
-# CompletionExecutor 클래스 정의
+#client_apik = os.environ.get('CLOVA_API_KEY')
+#client_api_primal = os.environ.get('CLOVA_API_KEY_PRIMARY_VAL')
+#client_required = os.environ.get('CLOVA_REQUEST_ID')
+client_id = st.secrets['NAVER_CLIENT_ID']
+client_secret = st.secrets['NAVER_CLIENT_SECRET']
+client_api = st.secrets['CLOVA_API_KEY']
+client_api_primal = st.secrets['CLOVA_API_KEY_PRIMARY_VAL']
+client_required = st.secrets['CLOVA_REQUEST_ID']
+
 class CompletionExecutor:
     def __init__(self, host, api_key, api_key_primary_val, request_id):
         self._host = host
@@ -18,123 +23,102 @@ class CompletionExecutor:
         self._api_key_primary_val = api_key_primary_val
         self._request_id = request_id
 
-    def _send_request(self, completion_request):
+    def execute(self, completion_request):
         headers = {
-            'Content-Type': 'application/json; charset=utf-8',
             'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
             'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
-            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
-        }
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }  
+        responses = requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
+                           headers=headers, json=completion_request, stream=True)
+        final_summary = ""
+        for line in responses.iter_lines():
+            if line:
+                try:
+                    line_data = line.decode("utf-8")
+                    # Extract JSON data after "data:"
+                    if line_data.startswith("data:"):
+                        data = json.loads(line_data[5:])
+                        # Collect "content" field from "message" role
+                        if "message" in data and data["message"]["role"] == "assistant":
+                            content = data["message"].get("content", "")
+                            final_summary += content
+                        # Check for the final result event
+                        if data.get("event") == "result":
+                            return data["message"]["content"]
+                except json.JSONDecodeError:
+                    continue
+        return final_summary.strip()
 
-        conn = http.client.HTTPSConnection(self._host)
-        conn.request('POST', '/testapp/v1/api-tools/summarization/v2/40b7767cc1d846e3bf2bde48ccb690a5', json.dumps(completion_request), headers)
-        response = conn.getresponse()
-        result = json.loads(response.read().decode(encoding='utf-8'))
-        conn.close()
-        return result
-    def execute_summarization(self, completion_request):
-        return self._send_request(completion_request)
-
-    def execute_translation(self, completion_request):
-        return self._send_request(completion_request)
-
-# 클로바 X 요약 함수
-def summarize_clova(text):
-    completion_executor = CompletionExecutor(
-        host='clovastudio.apigw.ntruss.com',
-        #api_key=os.environ.get('CLOVA_API_KEY'),
-        api_key = st.secrets['CLOVA_API_KEY'],
-        #api_key_primary_val=os.environ.get('CLOVA_API_KEY_PRIMARY_VAL'),
-        api_key_primary_val = st.secrets['CLOVA_API_KEY_PRIMARY_VAL'],
-        #request_id=os.environ.get('CLOVA_REQUEST_ID')
-        request_id = st.secrets['CLOVA_REQUEST_ID']
-    )
-
-    # Summarization requests
-    request_data = {
-        "texts": [text],
-        "segMinSize": 300,
-        "includeAiFilters": True,
-        "autoSentenceSplitter": True,
-        "segCount": -1,
-        "segMaxSize": 1000
-    }
-
-    summarization_response = completion_executor.execute_summarization(request_data)
-    if 'status' in summarization_response and summarization_response['status']['code'] == '20000':
-        return summarization_response['result']['text']
-    else:
-        return 'Summarization Error'
-
-def translate_clova(text):
-    completion_executor = CompletionExecutor(
-        host='clovastudio.apigw.ntruss.com',
-        api_key=os.environ.get('CLOVA_API_KEY'),
-        api_key_primary_val=os.environ.get('CLOVA_API_KEY_PRIMARY_VAL'),
-        request_id=os.environ.get('CLOVA_REQUEST_ID')
-    )
-
-    # Translation request using chat API
-    preset_text = [
-        {"role": "system", "content": "너는 최고의 영어 번역가야. 다음 한글을 영어로 자연스럽게 번역해줘."},
-        {"role": "user", "content": text}
-    ]
-
-    translate_request_data = {
-        'messages': preset_text,
-        'topP': 0.6,
-        'topK': 0,
-        'maxTokens': 512,
-        'temperature': 0.5,
-        'repeatPenalty': 5.0,
-        'stopBefore': [],
-        'includeAiFilters': True,
-        'seed': 0
-    }
-
-    translation_response = completion_executor.execute_translation(translate_request_data)
-    if 'choices' in translation_response:
-        return translation_response['result']['text']
-    else:
-        st.write("Translation:", translation_response)
-
-        return 'Translation Error'
-
-# 뉴스 검색 함수
-def search_news(query="계엄", display=8):
+def search_news(query="계엄", display=6):
     encoded_query = query
     url = f"https://openapi.naver.com/v1/search/news.json?query={encoded_query}&display={display}"
     headers = {
-        "X-Naver-Client-Id": client_id,
-        "X-Naver-Client-Secret": client_secret
+    "X-Naver-Client-Id": client_id,
+    "X-Naver-Client-Secret": client_secret
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return response.json().get('items', [])
+        items = response.json().get('items', [])
+        for item in items:
+            # HTML tag delete (<b>, </b>)
+            item['title'] = re.sub(r'<\/?b>', '', item['title'])
+            item['description'] = re.sub(r'<\/?b>', '', item['description'])
+        return items
     else:
         st.error(f"Failed to search news. Status code: {response.status_code}")
         return []
-
-# Streamlit 애플리케이션
+    
 st.title("What's happening in Korea? News related to martial law")
-st.write("This website is intended to share news related to the declaration of martial law in South Korea on December 2, 2024 I want to raise awareness about the critical situation in South Korea. Contact : stormkingrank1@gmail.com.")
-# 검색 버튼 클릭 횟수 제한
+st.write("On December 2, 2024, the President of South Korea declared martial law. In response, I created AI-powered service to provide real-time summaries of news related to the crisis from South Korea’s largest portal, NAVER, to inform the global audience accurately. This initiative aims to show that peace and love can triumph over violence, and invites support or inquiries at stormkingrank1@gmail.com.")
+# Click button restricted #3
+#if st.button("Search"):
+#    if st.session_state['search_count'] < 3:
+#        st.session_state['search_count'] += 1
+#        news_items = search_news()
+#        for item in news_items:
+#            #st.subheader(item['title'])
+#            #st.write(item['description'])
+#            newsitems = item['title']+item['description']
+#            result = completion_executor.execute(request_data)
+#            st.write("Summary:", result)
+#            st.write(f"[Original Article]({item['link']})")
+#    else:
+#        st.warning("You have reached the maximum number of searches (3).")
+if __name__ == '__main__':
+    completion_executor = CompletionExecutor(
+        host='https://clovastudio.stream.ntruss.com',
+        api_key= client_api,
+        api_key_primary_val=client_api_primal,
+        request_id=client_required
+        )
 if 'search_count' not in st.session_state:
     st.session_state['search_count'] = 0
-
 if st.button("Search"):
     if st.session_state['search_count'] < 3:
-        st.session_state['search_count'] += 1
+        st.session_state['search_count'] += 1        
         news_items = search_news()
+
         for item in news_items:
-            st.subheader(item['title'])
-            #st.write(item['description'])
-            # 클로바 X 요약 함수 사용
-            summary = summarize_clova(item['description'])
-            st.write("Summary:", summary)
-            # 클로바 X 번역 함수 사용
-            #translated_summary = translate_clova(summary)
-            #st.write("Translated Summary:", translated_summary)
+            describe = item['description']
+            preset_text = [{"role":"system","content":"- 당신은 텍스트 요약 어시스턴트입니다.\n- 주어진 텍스트의 내용을 분석하여 간결하고 명확한 3줄 요약을 제공합니다.\n- 다음 지침을 엄격히 준수하여 업무를 수행하십시오.\n- 주어진 텍스트의 전체적인 맥락과 주제를 파악합니다.\n- 먼저 기사 제목을 영어로 번역해 제공합니다.\n- 불필요한 세부사항은 제외하고 간결하고 명확한 문장으로 요약을 작성하세요.\n-  모든 응답은 영어로만 제공하세요.\n- 원문의 핵심을 정확히 반영하는 효과적인 요약을 만들어주세요.\n- 위의 지침을 철저히 따라 고품질의 텍스트 요약을 3줄로 제공합니다.\n###형식\n[TITLE]\n[content]\n###끝"},{"role":"user","content":describe}]
+            request_data = {
+                'messages': preset_text,
+                'topP': 0.8,
+                'topK': 0,
+                'maxTokens': 512,
+                'temperature': 0.1,
+                'repeatPenalty': 1.2,
+                'stopBefore': [],
+                'includeAiFilters': True,
+                'seed': 0
+            }       
+            
+            resultation = completion_executor.execute(request_data)
+            st.write("Summary:", resultation)
             st.write(f"[Original Article]({item['link']})")
     else:
         st.warning("You have reached the maximum number of searches (3).")
+# Streamlit 애플리케이션
